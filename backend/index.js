1256c6bbd6494c8302203188e02ca13169f51236
Amazon <kaushaljangid535@gmail.com>
2025-12-22 15:05:15 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/db.js";
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+  connectDb();
+});
